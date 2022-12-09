025ce75762ca2c63c95a0fc1b3f9600906013307
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-09 22:50:07 +0100

    JS - Variables & Data Types

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello, World');
+var life = 100; // this is the health bar number
+
+// 90
+life = life - 10;
+
+//
+life = life / 10;
+
+var name = "Yasser"; // this is the name displayed in the game
+
+var checkOut = false; // this is a boolean
+
+console.log(life);
