4a0b5e32f4972ac2bfc35f66a4e43fd7c4b5182f
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-09 23:01:29 +0100

    Create var.js

---
+++ var.js
@@ -0,0 +1,17 @@
+// Variables and Data types
+
+var life = 100; // this is the health bar number
+
+// 90
+life = life - 10;
+
+//
+life = life / 10;
+
+var name = "Yasser"; // this is the name displayed in the game
+
+var checkOut = false; // this is a boolean
+
+var box = null;
+
+console.log(box);
