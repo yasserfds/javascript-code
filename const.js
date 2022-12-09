c96afe4baca3f30e39616d83b20dabbbb29ad4c0
TheGiantNinja <yasserfedsi34@gmail.com>
2022-12-09 23:11:44 +0100

    Var, Const, Let Varibles

---
+++ const.js
@@ -0,0 +1,21 @@
+// A constantes can't be re-declared twice
+// Using const variable
+
+
+//const life = 100;
+// life = life - 10;
+
+// console.log(life)
+
+// Note: it will show an error on the console
+// Reason: because a constatnte can't be changed like variables
+
+
+
+// Using Let variable
+
+let life = 100;
+
+life = life - 10;
+
+console.log(life)
